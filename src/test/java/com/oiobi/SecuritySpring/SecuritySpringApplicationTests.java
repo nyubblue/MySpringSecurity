aab60e6b146df1d865b5744c9f1fc1d8e44d149e
thanhbuynitu <thanhbuynitu@gmail.com>
2022-06-28 03:19:28 +0700

    commit init code

---
+++ src/test/java/com/oiobi/SecuritySpring/SecuritySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oiobi.SecuritySpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuritySpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
